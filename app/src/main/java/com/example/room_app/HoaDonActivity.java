13ab80d1e6e654c18af6b78e088b0f970820de63
nhatpham <nhatphamlong.tt@gmail.com>
2022-05-25 16:17:50 +0700

    UI v1

---
+++ app/src/main/java/com/example/room_app/HoaDonActivity.java
@@ -0,0 +1,14 @@
+package com.example.room_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HoaDonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hoa_don);
+    }
+}
