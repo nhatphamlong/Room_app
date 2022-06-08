950fa4dd865ed61d46abeeee70e8148f63d66669
phmquan <phanhoangminhquan240402@gmail.com>
2022-06-08 23:17:12 +0700

    Hoan thien tinh nang dang ky tai khoan

--- app/src/main/java/com/example/room_app/DangkyActivity.java
+++ app/src/main/java/com/example/room_app/DangkyActivity.java
@@ -26,6 +26,24 @@ public class DangkyActivity extends AppCompatActivity {
         btnDKDangKy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+
+                String taikhoan = edtDKTaiKhoan.getText().toString();
+                String matkhau = edtDKMatKhau.getText().toString();
+
+
+                TaiKhoan taikhoan1 = CreateTaiKhoan();
+                if(taikhoan.equals("") || matkhau.equals("")){
+                    Toast.makeText(DangkyActivity.this,"Bạn chưa nhập đầy đủ thông tin",Toast.LENGTH_SHORT).show();
+                    Log.e("Thông báo : ","Bạn chưa nhập đầy đủ thông tin");
+                }
+                //Nếu đầy đủ thông tin
+                else{
+                    //Kiểm tra xem trùng tài khoản không để có thể hiển thị thông báo tài khoản trùng
+
+                    databaseQLPT.AddTaiKhoan(taikhoan1);
+                    Toast.makeText(DangkyActivity.this,"Đăng ký thành công ",Toast.LENGTH_SHORT).show();
+                }
             }
         });
         btnDKDangNhap.setOnClickListener(new View.OnClickListener() {
@@ -36,6 +54,14 @@ public class DangkyActivity extends AppCompatActivity {
         });
     }
 
+    private TaiKhoan CreateTaiKhoan(){
+        String taikhoan = edtDKTaiKhoan.getText().toString();
+        String matkhau = edtDKMatKhau.getText().toString();
+
+        TaiKhoan tk = new TaiKhoan(taikhoan,matkhau);
+
+        return tk;
+    }
 
     private void AnhXa() {
         edtDKMatKhau = findViewById(R.id.dkMatkhau);
